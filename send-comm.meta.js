bf0e8cffe931ef80f9495dbc53f679d8ffbc0560
BdiWaw <biuro@bdi.waw.pl>
2026-02-17 17:31:38 +0100

    zmiana serwera

--- send-comm.meta.js
+++ send-comm.meta.js
@@ -2,11 +2,11 @@
 // @id             iitc-plugin-send-comm-local
 // @name           IITC plugin: Send COMM to local API (raw)
 // @category       Info
-// @version        0.2.1
+// @version        0.2.2
 // @description    Send ALL COMM raw events ([guid, ts_ms, {plext}]) to local FastAPI via GM_xmlhttpRequest
 // @match          https://intel.ingress.com/*
 // @grant          GM_xmlhttpRequest
 // @grant          unsafeWindow
-// @connect        127.0.0.1
+// @connect        srv42.mikr.us:20214
 // @connect        localhost
 // ==/UserScript==
--- send-comm.user.js
+++ send-comm.user.js
@@ -2,7 +2,7 @@
 // @id             iitc-plugin-send-comm-local
 // @name           IITC plugin: Send COMM to local API (raw)
 // @category       Info
-// @version        0.2.1
+// @version        0.2.2
 // @description    Send ALL COMM raw events ([guid, ts_ms, {plext}]) to local FastAPI via GM_xmlhttpRequest
 // @match          https://intel.ingress.com/*
 // @grant          GM_xmlhttpRequest
