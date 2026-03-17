f3fb14fe42dd257646ece939a0832344a2f6e7a3
BdiWaw <biuro@bdi.waw.pl>
2026-03-17 16:54:43 +0100

    poprawki

--- send-comm.meta.js
+++ send-comm.meta.js
@@ -4,11 +4,10 @@
 // @category       Info
 // @updateURL      https://github.com/BDIRepo/Send-portals/raw/master/send-comm.meta.js
 // @downloadURL    https://github.com/BDIRepo/Send-portals/raw/master/send-comm.user.js
-// @version        0.2.5
+// @version        0.2.6
 // @description    Send ALL COMM raw events ([guid, ts_ms, {plext}]) to local FastAPI via GM_xmlhttpRequest
 // @match          https://intel.ingress.com/*
 // @grant          GM_xmlhttpRequest
 // @grant          unsafeWindow
-// @connect        srv42.mikr.us:20214
 // @connect        srv42.mikr.us
 // ==/UserScript==
